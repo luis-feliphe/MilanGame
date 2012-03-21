aaa62bd0bacf709814b8d7d59985fa4578c8f155
unknown <Luis F@.(none)>
2012-03-21 00:32:42 -0300

    adicionando o arquivo Facade.py

---
+++ Facade.py
@@ -0,0 +1,49 @@
+class Facade:
+
+	#cria inimigo em determinado ponto da tela
+	def criarInimigo(posX, posY):
+		return
+
+	#move todos inimigos aleatoriamente na tela
+	def moverInimigos():
+		return
+
+	#adiciona nome no ranking 
+	def adicionarNomeNoRanking(nome):
+		return
+
+	#libera mais uma arma para jogador(modificado nome no caso de uso)
+	def liberarArma(tipoArma):
+		return
+
+	#muda arma que o jogador está usando
+	def mudarArma(arma):
+		return
+
+	#retorna string com o ranking (nomes de jogadores separados por *
+	def verRanking():
+		return
+
+	#nova partida
+	def iniciarJogo():
+		return
+
+	#muda posição do jogador
+	def moverJogador(posX, posY):
+		return
+
+	#jogador atira no inimigo
+	def atacarInimigo(posX, posY):
+		return
+	
+	#retorna String com as instruções do jogo
+	def verInstrucoes():
+		return
+
+	#encerra partida
+	def sairDoJogo():
+		return
+
+	#inimigo ataca jogador
+	def atacarJogador(posX, posY):
+		return
